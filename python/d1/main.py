8273daec282eada0e4f729b1b614aff79204a7f0
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-01 20:25:44 +0000

    Day 1 part 2 done
    Refactored so the same function runs both parts

--- python/d1/main.py
+++ python/d1/main.py
@@ -1,21 +1,35 @@
+from typing import List
+
 def main():
     d1_part1()
+    d1_part2()
 
 def d1_part1():
-    with open('input_1', 'r') as f:
+    track_highs('input_1', 1)
+
+def d1_part2():
+    track_highs('input_1', 3)
+
+def track_highs(file, to_track):
+    with open(file, 'r') as f:
         current = 0
-        highest = 0
+        tracked: List[int] = []
         
         for line in f:
             stripped = line.strip()
             if stripped == "":
-                highest = max(current, highest)
+                if len(tracked) < to_track or tracked[0] < current:
+                    tracked.append(current)
+                    tracked.sort()
+                    if len(tracked) > to_track:
+                        tracked.pop(0)
                 current = 0
             else:
                 current += int(stripped)
 
-        highest = max(highest, current)
-        print(highest)
+        print(tracked)
+        print(sum(tracked))
+
 
 if __name__ == "__main__":
     main()
