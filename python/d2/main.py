29daa6afd8f78d0884a7c76579578c5c93885561
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-02 08:54:03 +0000

    Day 2 solutions

---
+++ python/d2/main.py
@@ -0,0 +1,56 @@
+def main():
+    d2_part1()
+    d2_part2()
+
+def d2_part1():
+    print(get_score("input_1", strat1))
+
+def get_score(file, strat):
+    with open(file, 'r') as f:
+        return sum(strat(line.strip()) for line in f)
+
+def strat1(line):
+    if line == "A X":
+        return 4
+    elif line == "A Y":
+        return 8
+    elif line == "A Z":
+        return 3
+    elif line == "B X":
+        return 1
+    elif line == "B Y":
+        return 5
+    elif line == "B Z":
+        return 9
+    elif line == "C X":
+        return 7
+    elif line == "C Y":
+        return 2
+    elif line == "C Z":
+        return 6
+
+def d2_part2():
+    print(get_score("input_1", strat2))
+
+def strat2(line):
+    if line == "A X":
+        return 3
+    elif line == "A Y":
+        return 4
+    elif line == "A Z":
+        return 8
+    elif line == "B X":
+        return 1
+    elif line == "B Y":
+        return 5
+    elif line == "B Z":
+        return 9
+    elif line == "C X":
+        return 2
+    elif line == "C Y":
+        return 6
+    elif line == "C Z":
+        return 7
+
+if __name__ == "__main__":
+    main()
