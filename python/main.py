7f28e0345468729a504b8d49f30a944cb61eb21c
Barnes, Andrew <andrew.barnes@troweprice.com>
2022-12-05 17:09:43 +0000

    Add day 5 to top level and improve title formatting

--- python/main.py
+++ python/main.py
@@ -2,15 +2,17 @@ from d1 import d1
 from d2 import d2
 from d3 import d3
 from d4 import d4
+from d5 import d5
 
 def main():
     day("Calorelfic", lambda: d1("d1/input_1"))
     day("Rock Papelf Scissors", lambda: d2("d2/input_1"))
     day("Prielfitized Bags", lambda: d3("d3/input_1"))
     day("Santa's little cleaners", lambda: d4("d4/input_1"))
+    day("Contain yourelf", lambda: d5("d5/input_1", 9))
 
 def day(banner, exec):
-    print(f'{banner:=^60}')
+    print(f'{" " + banner + " ":=^60}')
     exec()
 
 if __name__ == "__main__":
