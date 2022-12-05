c53b63872d9eb269594689521453bc2df47de5a4
Barnes, Andrew <andrew.barnes@troweprice.com>
2022-12-05 16:22:10 +0000

    Add day 5 part 2 solution and test input

--- python/d5/main.py
+++ python/d5/main.py
@@ -1,12 +1,13 @@
 import re
 
 def main():
+    d5('input_test', 3)
     d5('input_1', 9)
 
 def d5(file, size):
     instruction_pat = re.compile(r"move (?P<num>\d+) from (?P<from>\d+) to (?P<to>\d+)")
     stack_pat = re.compile(r".([^1o])" + r"...(.)" * (size  - 1) + ".")
-    stacks = [[], [], [], [], [], [], [], [], []]
+    stacks = [[] for _ in range(0, size)]
 
     with open(file, 'r') as f:
         lines = [line for line in f]
@@ -20,10 +21,12 @@ def d5(file, size):
             num = int(match.group("num"))
             fr = int(match.group("from")) - 1
             to = int(match.group("to")) - 1
+            in_point = len(stacks[to])
             for i in range(0, num, 1):
-                stacks[to].append(stacks[fr].pop())
+                #stacks[to].append(stacks[fr].pop())
+                stacks[to].insert(in_point, stacks[fr].pop())
     
-    last = [stack[-1] for stack in stacks]
+    last = [stack[-1] for stack in stacks if (len(stack))]
     print(''.join(last))
 
 if __name__ == "__main__":
