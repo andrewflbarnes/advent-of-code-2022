d9bb1bd18f5bb211923255c522bf8b466de0614a
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-06 07:49:16 +0000

    Day 6 part 2 done

--- python/d6/main.py
+++ python/d6/main.py
@@ -5,14 +5,18 @@ def main():
     d6('input_1')
 
 def d6(file):
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding="UTF=8") as f:
         lines = [line for line in f]
-    
-    for line in lines:
-        groups = [line[x:x+4] for x in range(0, len(line) - 3)]
 
-        unique_indexes = [(i + 4) for i in range(0, len(groups)) if (len(set(groups[i])) == 4)]
-        print(unique_indexes[0])
+    for line in lines:
+        unique_occurrence(line, 4)
+        unique_occurrence(line, 14)
+
+def unique_occurrence(line, size):
+    groups = [line[x:x+size] for x in range(0, len(line) - size + 1)]
+
+    unique_indexes = [(i + size) for i in range(0, len(groups)) if len(set(groups[i])) == size]
+    print(unique_indexes[0])
 
 if __name__ == "__main__":
     main()
