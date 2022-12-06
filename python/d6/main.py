e1d77e7c826f1286927e93381d7e87d89b3dc862
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-06 07:57:21 +0000

    Remove unnecessary d6 import and format

--- python/d6/main.py
+++ python/d6/main.py
@@ -1,8 +1,7 @@
-import re
-
 def main():
     d6('input_test')
     d6('input_1')
+
 
 def d6(file):
     with open(file, 'r', encoding="UTF=8") as f:
@@ -12,11 +11,14 @@ def d6(file):
         unique_occurrence(line, 4)
         unique_occurrence(line, 14)
 
+
 def unique_occurrence(line, size):
     groups = [line[x:x+size] for x in range(0, len(line) - size + 1)]
 
-    unique_indexes = [(i + size) for i in range(0, len(groups)) if len(set(groups[i])) == size]
+    unique_indexes = [(i + size) for i in range(0, len(groups))
+                      if len(set(groups[i])) == size]
     print(unique_indexes[0])
+
 
 if __name__ == "__main__":
     main()
