87bc7d8cb78768b598d53abb3658591dd0874714
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-06 07:51:12 +0000

    Add day 6 to top level

--- python/main.py
+++ python/main.py
@@ -3,6 +3,7 @@ from d2 import d2
 from d3 import d3
 from d4 import d4
 from d5 import d5
+from d6 import d6
 
 def main():
     day("Calorelfic", lambda: d1("d1/input_1"))
@@ -10,6 +11,7 @@ def main():
     day("Prielfitized Bags", lambda: d3("d3/input_1"))
     day("Santa's little cleaners", lambda: d4("d4/input_1"))
     day("Contain yourelf", lambda: d5("d5/input_1", 9))
+    day("Signelf processing", lambda: d6("d6/input_1"))
 
 def day(banner, exec):
     print(f'{" " + banner + " ":=^60}')
