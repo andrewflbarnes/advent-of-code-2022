aafda537e50c56a493f12aee8be1229316791f11
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-14 23:22:40 +0000

    Day 14 refactor for part 2

--- python/d14/main.py
+++ python/d14/main.py
@@ -5,13 +5,39 @@ def main():
     d14('input_1')
 
 debug = False
-debug_steps = None
+debug_steps = 0
 sand_src = 500
 
 def d14(file):
     with open(file, "r", encoding="utf-8") as f:
         lines = [[list(map(int, coords.split(","))) for coords in line.strip().split(" -> ")] for line in f]
 
+    paths = get_paths(lines)
+    x_min, x_max, _, y_max = get_min_max(paths)
+
+    sand_drop = sand_src - x_min
+
+    # part 1
+    pour_sand(paths, sand_drop, x_min, x_max, y_max, False)
+    # part 2
+    pour_sand(paths, sand_drop, x_min, x_max, y_max, True)
+
+def pour_sand(paths, sand_drop, x_min, x_max, y_max, has_floor):
+    rocks = set([(point[0]-x_min, point[1]) for path in paths for point in path])
+
+    debug_step = 0
+    settled = 0
+    new_settled = 1
+    sands = []
+    while new_settled > 0:
+        new_settled = drop_sand(rocks, sands, sand_drop, x_min, x_max, y_max)
+        settled += new_settled
+        debug_step += 1
+        if debug_steps > 0 and debug_step >= debug_steps:
+            break
+    print(settled)
+
+def get_paths(lines):
     paths = []
     for line in lines:
         path = []
@@ -23,35 +49,14 @@ def d14(file):
             coords = [[x,y] for y in range(start[1], end[1]+y_dir, y_dir) for x in range(start[0], end[0]+x_dir, x_dir)]
             path.extend(coords[0 if i == 0 else 1:])
         paths.append(path)
+    return paths
 
-    # for p in paths:
-    #     print(p)
-
-    x_max = max([max([point[0] for point in path]) for path in paths])
+def get_min_max(paths):
     x_min = min([min([point[0] for point in path]) for path in paths])
-    # print(f'x from {x_min} to {x_max}')
-
+    x_max = max([max([point[0] for point in path]) for path in paths])
+    y_min = min([min([point[1] for point in path]) for path in paths])
     y_max = max([max([point[1] for point in path]) for path in paths])
-    # y_min = min([min([point[1] for point in path]) for path in paths])
-    # print(f'x from {x_min} to {x_max}')
-    # print(f'y from {y_min} to {y_max}')
-
-    rocks = set([(point[0]-x_min, point[1]) for path in paths for point in path])
-    # print(rocks)
-    sand_drop = sand_src - x_min
-
-
-    debug_step = 0
-    settled = True
-    sands = []
-    while settled:
-        settled = drop_sand(rocks, sands, sand_drop, x_min, x_max, y_max)
-        debug_step += 1
-        if debug_steps is not None and debug_step >= debug_steps:
-            break
-        # frame(rocks, sands, y_max, x_min, x_max)
-        # break
-    print(len(sands))
+    return (x_min, x_max, y_min, y_max)
 
 def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
     sand = (sand_drop, -1)
@@ -61,7 +66,7 @@ def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
         frame(rocks, sands, y_end, x_start, x_end, sand)
         next_y = sand[1] + 1
         if next_y > y_end:
-            return False
+            return 0
         
         check = (x, next_y)
         if check not in sands and check not in rocks:
@@ -70,7 +75,7 @@ def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
 
         for next_x in [x - 1, x + 1]:
             if next_x < 0 or next_x > x_end:
-                return False
+                return 0
 
             check = (next_x, next_y)
             if check not in sands and check not in rocks:
@@ -78,7 +83,7 @@ def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
                 break
         else:
             sands.append(sand)
-            return True
+            return 1
 
 
 def frame(rocks, sands, y_end, x_start, x_end, s = (-1, -1)):
