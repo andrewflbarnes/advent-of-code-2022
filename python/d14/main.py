d0f7d31992a4dea099c6a7feb34f80ca24295876
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-15 00:18:44 +0000

    Day 14 day 2
    Didn't realise for a while I was storing sand positions in a list and
    not a set which was preventing this from completing in any meaninful
    time.

--- python/d14/main.py
+++ python/d14/main.py
@@ -4,8 +4,6 @@ def main():
     d14('input_test')
     d14('input_1')
 
-debug = False
-debug_steps = 0
 sand_src = 500
 
 def d14(file):
@@ -22,20 +20,16 @@ def d14(file):
     # part 2
     pour_sand(paths, sand_drop, x_min, x_max, y_max, True)
 
-def pour_sand(paths, sand_drop, x_min, x_max, y_max, has_floor):
+def pour_sand(paths, sand_drop, x_min, x_max, y_end, has_floor):
+    y_max = y_end + 2 if has_floor else y_end
     rocks = set([(point[0]-x_min, point[1]) for path in paths for point in path])
 
-    debug_step = 0
-    settled = 0
-    new_settled = 1
-    sands = []
-    while new_settled > 0:
-        new_settled = drop_sand(rocks, sands, sand_drop, x_min, x_max, y_max)
-        settled += new_settled
-        debug_step += 1
-        if debug_steps > 0 and debug_step >= debug_steps:
-            break
-    print(settled)
+    sands = set()
+    while not drop_sand(rocks, sands, sand_drop, x_max, y_max, has_floor):
+        # frame(rocks, sands, y_end, x_min, x_max, has_floor)
+        pass
+    # frame(rocks, sands, y_end, x_min, x_max, has_floor)
+    print(len(sands))
 
 def get_paths(lines):
     paths = []
@@ -58,15 +52,18 @@ def get_min_max(paths):
     y_max = max([max([point[1] for point in path]) for path in paths])
     return (x_min, x_max, y_min, y_max)
 
-def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
+def drop_sand(rocks, sands, sand_drop, x_end, y_end, has_floor):
     sand = (sand_drop, -1)
     while True:
-        x = sand[0]
-        # print(f'{sand} {sands}')
-        frame(rocks, sands, y_end, x_start, x_end, sand)
-        next_y = sand[1] + 1
-        if next_y > y_end:
-            return 0
+        x, y = sand
+        # frame(rocks, sands, y_end, x_start, x_end, has_floor, sand)
+        next_y = y + 1
+        if has_floor:
+            if next_y >= y_end:
+                sands.add(sand)
+                return False
+        elif next_y > y_end:
+            return True
         
         check = (x, next_y)
         if check not in sands and check not in rocks:
@@ -74,25 +71,28 @@ def drop_sand(rocks, sands, sand_drop, x_start, x_end, y_end):
             continue
 
         for next_x in [x - 1, x + 1]:
-            if next_x < 0 or next_x > x_end:
-                return 0
+            if has_floor:
+                pass
+            elif next_x < 0 or next_x > x_end:
+                return True
 
             check = (next_x, next_y)
             if check not in sands and check not in rocks:
                 sand = check
                 break
         else:
-            sands.append(sand)
-            return 1
+            sands.add(sand)
+            return has_floor and sand[1] == 0
 
 
-def frame(rocks, sands, y_end, x_start, x_end, s = (-1, -1)):
-    if not debug:
-        return
+def frame(rocks, sands, y_end, x_start, x_end, has_floor, s = (-1, -1)):
     for y in range(0, y_end+1):
         for x in range(0, x_end-x_start+1):
-            p = (x,y)
-            print("o" if p in sands or p == s else "#" if p in rocks else ".", end="")
+            if has_floor and y == y_end:
+                print("#", end="")
+            else:
+                p = (x,y)
+                print("o" if p in sands or p == s else "#" if p in rocks else ".", end="")
         print()
     print()
 
