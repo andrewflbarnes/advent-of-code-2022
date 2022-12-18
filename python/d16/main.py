9cc5b6c6f571cb42d00bb2ef5c5b72dfdf8dcae3
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-18 16:05:30 +0000

    Day 16 part 1 test working

---
+++ python/d16/main.py
@@ -0,0 +1,83 @@
+import re
+
+def main():
+    d16('input_test')
+    d16('input_1')
+
+re_line = re.compile(r"Valve (\w{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
+
+class Valve:
+    def __init__(self, args):
+        name, rate, links = args
+        self.name = name
+        self.rate = rate
+        self.links = links
+        self.linked: dict[str: (int, Valve)] = dict()
+
+    def __repr__(self) -> str:
+        return f'Valve {self.name}, flow rate {self.rate}, linked to {[(k, v[0]) for k, v in self.linked.items()]}'
+
+def d16(file):
+    with open(file, "r", encoding="utf-8") as f:
+        valve_args = [(groups[0], int(groups[1]), groups[2].split(", "))
+                  for line in f
+                  if (groups := re_line.match(line).groups())]
+
+    valves = {valve[0]: Valve(valve) for valve in valve_args}
+    openable = [v for v in valves if valves[v].rate > 0]
+    print(openable)
+    link_valves(valves)
+    rate, path = best_sub_rate(valves["AA"], openable, 30)
+    print(f'{rate} {path}')
+
+def link_valves(valves: dict[str, Valve]):
+    for v_name, v in valves.items():
+        dist = 1
+        next = [(dist, l) for l in v.links]
+        while len(next):
+            dist += 1
+            curr = next
+            next = []
+            for c_dist, c_valve_name in curr:
+                if c_valve_name == v_name:
+                    continue
+                if c_valve_name in v.linked:
+                    continue
+                
+                c_valve = valves[c_valve_name]
+                v.linked[c_valve_name] = (c_dist, c_valve)
+                next.extend([(dist, l) for l in c_valve.links])
+
+def best_sub_rate(valve: Valve, openable, time):
+    if len(openable) <= 0 or time <= 0:
+        return 0, None
+    
+    if len(openable) == 1:
+        open_valve_name = openable[0]
+        dist, open_valve = valve.linked[open_valve_name]
+        if dist < time:
+            return (time - dist - 1) * open_valve.rate, [open_valve_name]
+        else:
+            return 0, None
+
+    best = 0, None
+    for open in openable:
+        v_dist, next_v = valve.linked[open]
+        r, p = best_sub_rate(valve, [open], time)
+        if p is None:
+            continue
+        sub_r, sub_p = best_sub_rate(next_v, list(filter(lambda x : x != open, openable)), time - 1 - v_dist)
+        best_r, _ = best
+        if best_r < r + sub_r:
+            print(f'{r} + {sub_r}, {p} + {sub_p}')
+            best = r + sub_r, p + sub_p
+
+    return best
+
+    
+    # for n_name, n_valve in valve.linked.items():
+            
+
+    # print(re_line.match(valves[0]).groups())
+if __name__ == "__main__":
+    main()
