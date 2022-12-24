46b646f152171ce8a67fe6dccb98aa4c33833c07
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2022-12-24 01:24:14 +0000

    Day 23 part 2

--- python/d23/main.py
+++ python/d23/main.py
@@ -11,9 +11,11 @@ def d23(file, iterations):
              for x in range(len(raw[0])) if raw[y][x] == 1}
 
     dir = 0  # 0:N, 1:S, 2:W, 3:E
-    # frame(elves)
-    for _ in range(iterations):
-        # print(f'iteration {_} with dir {dir}')
+    it = 0
+    iter_done = False
+    moving = True
+    while not iter_done or moving:
+        it += 1
         may_move = {e for e in elves if sum(1 if (x, y) in elves and (
             x, y) != e else 0 for x in range(e[0] - 1, e[0] + 2) for y in range(e[1] - 1, e[1] + 2)) > 0}
         proposed = dict()
@@ -22,12 +24,17 @@ def d23(file, iterations):
                 if move is None:
                     raise ValueError("BLOOP")
                 proposed[move] = None if move in proposed else e
-        elves = {e for e in elves if e not in may_move}\
+        next_elves = {e for e in elves if e not in may_move}\
             .union({e for e in may_move if e not in proposed.values()})\
             .union({to for to, frm in proposed.items() if frm is not None})
+        if next_elves == elves:
+            print(f'Elves stopped moving after {it} iterations')
+            moving = False
+        elves = next_elves
         dir = (dir + 1) % 4
-        # frame(elves)
-    print(count_elf_rect(elves))
+        if it == iterations:
+            print(f'empty after {it} iterations: {count_elf_rect(elves)}')
+            iter_done = True
 
 
 elf_checks = {
@@ -46,14 +53,12 @@ elf_propose = {
 
 def propose_move(dir, elf, elves):
     x, y = elf
-    # print(f'check elf {elf}')
     for _ in range(4):
-        # print(f'{dir}: {elf_checks[dir](x, y)}')
         if sum(1 if c in elves else 0 for c in elf_checks[dir](x, y)) == 0:
-            # print(f'proposing {elf_propose[dir](x, y)}')
             return elf_propose[dir](x, y)
         dir = (dir + 1) % 4
     return None
+
 
 def count_elf_rect(elves):
     min_x = min(x for x, _ in elves)
@@ -61,19 +66,6 @@ def count_elf_rect(elves):
     min_y = min(y for _, y in elves)
     max_y = max(y for _, y in elves)
     return (max_x + 1 - min_x) * (max_y + 1 - min_y) - len(elves)
-
-
-def frame(elves):
-    min_x = min(x for x, _ in elves)
-    max_x = max(x for x, _ in elves)
-    min_y = min(y for _, y in elves)
-    max_y = max(y for _, y in elves)
-
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            print("#" if (x, y) in elves else ".", end="")
-        print()
-    print()
 
 
 if __name__ == "__main__":
